3fd80282ae59681684cf5b9759ee3d4a66587e38
Ammar VSC <ammarmgmv@gmail.com>
2022-04-07 02:55:32 +0100

    UI message

--- src/userInterface.java
+++ src/userInterface.java
@@ -2,6 +2,11 @@ public class userInterface
 {
 	public static void main(String[] args) 
 	{
-        System.out.println("Testing User Interface"); 
+        System.out.println("Please select one of the options below:"); 
+        System.out.println("Press 1 to find the shortest path between two bus stops");
+        System.out.println("Press 2 to get information on a specific bus stop");
+        System.out.println("Press 3 to search for all trips with a given arrival time");
+        System.out.println("Press 0 to exit the program");
+        
     }
 }
